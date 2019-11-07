3a6b0b19d0817ede6c38da632ca0f87d0f826ae9
Kaito Koike <kaikai.poteti@keio.jp>
2019-11-07 12:01:55 +0900

    画像保存用のプログラム作成

---
+++ others/save_image.py
@@ -0,0 +1,57 @@
+from .. import pose_detector
+from .. import hand_detector
+import cv2
+import argparse
+import chainer
+import cupy as cp
+import time
+chainer.using_config('enable_backprop', False)
+pool = cp.cuda.MemoryPool(cp.cuda.malloc_managed)
+cp.cuda.set_allocator(pool.malloc)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='hand detector')
+    parser.add_argument('--gpu', '-g', type=int, default=-1, help='GPU ID (negative value indicates CPU)')
+    parser.add_argument("--precise", "-p", type=str, default=False, help="pose detector mode (precise or not)")
+    parser.add_argument("--cameraId", "-c", type=int, default=0, help="select an id of camera you will use")
+    args = parser.parse_args()
+
+    # load model
+    hand_detector = hand_detector.HandDetector("handnet", "../models/handnet.npz", device=args.gpu)
+    pose_detector = pose_detector.PoseDetector("posenet", "../models/coco_posenet.npz", device=args.gpu, precise=args.precise)
+    cap = cv2.VideoCapture(args.cameraId)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+    while True:
+        # get video frame
+        ret, img = cap.read()
+
+        if not ret:
+            print("Failed to capture image")
+            break
+
+        person_pose_array, _ = pose_detector(img)
+        res_img = cv2.addWeighted(img, 0.6, pose_detector.draw_person_pose(img, person_pose_array), 0.4, 0)
+        for person_pose in person_pose_array:
+            unit_length = pose_detector.get_unit_length(person_pose)
+
+            # hands estimation
+            hands = pose_detector.crop_hands(img, person_pose, unit_length)
+            if hands["left"] is not None:
+                hand_img = hands["left"]["img"]
+                bbox = hands["left"]["bbox"]
+                hand_keypoints = hand_detector(hand_img, hand_type="left")
+                res_img = hand_detector.draw_hand_keypoints(res_img, hand_keypoints, (bbox[0], bbox[1]))
+
+            if hands["right"] is not None:
+                hand_img = hands["right"]["img"]
+                bbox = hands["right"]["bbox"]
+                hand_keypoints = hand_detector(hand_img, hand_type="right")
+                res_img = hand_detector.draw_hand_keypoints(res_img, hand_keypoints, (bbox[0], bbox[1]))
+
+        cv2.imshow("result", res_img)
+        result_file_name = "result_hand"+time.time()+".png"
+        cv2.imWrite(result_file_name, res_img)
+        cv2.waitKey(1)

da3fae1f62e3bf3f6083a1f660e1b3689430f094
Kaito Koike <kaikai.poteti@keio.jp>
2019-11-17 22:55:53 +0900

    train作成のファイル追加

--- others/save_image.py
+++ others/save_image.py
@@ -18,17 +18,25 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='hand detector')
     parser.add_argument('--gpu', '-g', type=int, default=-1, help='GPU ID (negative value indicates CPU)')
     parser.add_argument("--precise", "-p", type=str, default=False, help="pose detector mode (precise or not)")
-    parser.add_argument("--cameraId", "-c", type=int, default=0, help="select an id of camera you will use")
+    parser.add_argument("--video_path", "-vp", type=str, default=None, help="video path to make a data")
     args = parser.parse_args()
 
     # load model
     hand_detector = HandDetector("handnet", "../models/handnet.npz", device=args.gpu)
     pose_detector = PoseDetector("posenet", "../models/coco_posenet.npz", device=args.gpu, precise=args.precise)
-    cap = cv2.VideoCapture(args.cameraId)
+
+    # ビデオをキャプチャーして解析し，保存する
+    cap = cv2.VideoCapture(args.video_path)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+    if not cap.isOpened():
+        sys.exit()
+    train_file = open("train.csv", "a")
+    train_file.write("hand_position,right_hand,left_hand")
+
     while True:
+
         # get video frame
         ret, img = cap.read()
 
@@ -56,14 +64,27 @@ if __name__ == '__main__':
                 bbox = hands["right"]["bbox"]
                 hand_keypoints = hand_detector(hand_img, hand_type="right")
                 res_img = draw_hand_keypoints(res_img, hand_keypoints, (bbox[0], bbox[1]))
+        print(hands_result)
 
         now_time = time.time()
         cv2.imshow("result", res_img)
+
         result_file_path_name = "result_images/result_hand"+str(now_time)+".png"
         hands_position_file_path = "result_hand_position/result_hand_pos"+str(now_time)+".json"
         hands_file = open(hands_position_file_path, "w")
         json.dump(hands_result, hands_file)
+
+        cv2.imshow("result_image", res_img)
+        print("これは意見なら1，質問なら2，それ以外なら0を記入")
+        print("右")
+        train_right = input()
+        print("左")
+        train_left = input()
+        train_line = "\n"+hands_position_file_path+","+train_right+","+train_left
+        train_file.write(train_line)
+
         ret = cv2.imwrite(result_file_path_name, res_img)
+
         if not ret:
             print("fail to save this image")
             break

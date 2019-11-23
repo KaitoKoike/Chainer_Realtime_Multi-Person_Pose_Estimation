7e0a32fd8fcd607a388938ce768361e03bbe68b3
Kaito Koike <kaikai.poteti@keio.jp>
2019-11-23 21:22:16 +0900

    モデル追加

--- camera_hand_demo.py
+++ camera_hand_demo.py
@@ -3,10 +3,11 @@ import argparse
 import chainer
 from pose_detector import PoseDetector, draw_person_pose
 from hand_detector import HandDetector, draw_hand_keypoints
-import cupy as cp
-chainer.using_config('enable_backprop', False)
-pool = cp.cuda.MemoryPool(cp.cuda.malloc_managed)
-cp.cuda.set_allocator(pool.malloc)
+from gesture_recognizer import GestureRecognizer, draw_gesture
+#import cupy as cp
+#chainer.using_config('enable_backprop', False)
+#pool = cp.cuda.MemoryPool(cp.cuda.malloc_managed)
+#cp.cuda.set_allocator(pool.malloc)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='hand detector')
@@ -18,9 +19,11 @@ if __name__ == '__main__':
     # load model
     hand_detector = HandDetector("handnet", "models/handnet.npz", device=args.gpu)
     pose_detector = PoseDetector("posenet", "models/coco_posenet.npz", device=args.gpu, precise=args.precise)
+    gesture_recognizer =GestureRecognizer(model_path="models/gesture_recog_model.pkl")
     cap = cv2.VideoCapture(args.cameraId)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    print("camera captured")
 
     while True:
         # get video frame
@@ -37,17 +40,24 @@ if __name__ == '__main__':
 
             # hands estimation
             hands = pose_detector.crop_hands(img, person_pose, unit_length)
+            hand_gesture_right = 0
+            hand_hesture_left = 0
             if hands["left"] is not None:
                 hand_img = hands["left"]["img"]
                 bbox = hands["left"]["bbox"]
                 hand_keypoints = hand_detector(hand_img, hand_type="left")
                 res_img = draw_hand_keypoints(res_img, hand_keypoints, (bbox[0], bbox[1]))
+                hand_gesture_right = gesture_recognizer(hand_keypoints)
+                res_img = draw_gesture(res_img,hand_gesture_right,(bbox[0],bbox[1]))
 
             if hands["right"] is not None:
                 hand_img = hands["right"]["img"]
                 bbox = hands["right"]["bbox"]
                 hand_keypoints = hand_detector(hand_img, hand_type="right")
                 res_img = draw_hand_keypoints(res_img, hand_keypoints, (bbox[0], bbox[1]))
+                hand_gesture_left = gesture_recognizer(hand_keypoints)
+                res_img = draw_gesture(res_img, hand_gesture_left, (bbox[0], bbox[1]))
+
 
         cv2.imshow("result", res_img)
         cv2.waitKey(1)
---
+++ gesture_recognizer.py
@@ -0,0 +1,44 @@
+import pickle
+import cv2
+import numpy as np
+
+
+class GestureRecognizer(object):
+    def __init__(self, model_path=None):
+        print('Loading GestureModel...')
+        print(model_path)
+        self.model = pickle.load(open(model_path, "rb"))
+
+    def __call__(self, hand_keypoint):
+        hand_keypoint = self.preprocessing(hand_keypoint)
+        gesture = self.model.predict([hand_keypoint])
+        print(gesture)
+        return gesture
+
+    def preprocessing(self, hand_key_point):
+        none_indexes = [i for i, hand_position in enumerate(hand_key_point) if hand_position is None]
+        for idx in none_indexes:
+            hand_key_point[idx] = [0.0, 0.0, 0.0]
+        base_keypoint = hand_key_point[0]
+        base_keypoint_x, base_keypoint_y = base_keypoint[0], base_keypoint[1]
+        keypoint_from_base = []
+        for i, position in enumerate(hand_key_point):
+            position_x, position_y = position[0], position[1]
+            position_x -= base_keypoint_x
+            position_y -= base_keypoint_y
+            keypoint_from_base.append([position_x, position_y])
+
+        return np.array(keypoint_from_base).ravel()
+
+
+def draw_gesture(img,gesture,left_top):
+    left, top = left_top
+    action = None
+    if gesture == "0":
+        action = "nothing"
+    elif gesture == "1":
+        action = "opinion"
+    elif gesture == "2":
+        action = "question"
+    cv2.putText(img, action, (left,top), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), lineType=cv2.LINE_AA)
+    return img

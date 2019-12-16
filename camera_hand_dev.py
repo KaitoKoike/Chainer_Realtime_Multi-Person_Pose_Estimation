0dc6fc0da03a7cc23017c993145f605ec388a46e
小池開人 <koikekaito@kaito-mac.local>
2019-12-17 04:02:32 +0900

    例外を受け取る様に変更

--- camera_hand_dev.py
+++ camera_hand_dev.py
@@ -75,7 +75,11 @@ if __name__ == '__main__':
             data: "{0},{1}" 
             """.format(speaker_id+1,student_status)
             query = {"message":message,'topic_name':'/printeps/std_msgs/update_student_status'}
-            requests.post("http://surface01.local:8080/publish",data=query)
+            try:
+                requests.post("http://surface01.local:8080/publish",data=query)
+            except Exception as e:
+                print(e)
+
         if args.mode == "camera":
             cv2.imshow("result", res_img)
         #time.sleep(0.1)

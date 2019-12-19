d845a7d9b2c7619601bc9589b78b4d7aa746a46a
小池開人 <kaikai.poteti@keio.jp>
2019-12-19 16:10:11 +0900

    framerateの変更

--- camera_hand_dev.py
+++ camera_hand_dev.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     pose_detector = PoseDetector("posenet", "models/coco_posenet.npz", device=args.gpu)
     right_gesture_recognizer = GestureRecognizer(model_path="models/right_gesture_recog_model.pkl")
     left_gesture_recognizer = GestureRecognizer(model_path="models/left_gesture_recog_model.pkl")
-    cap = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)720, height=(int)320,format=(string)NV12, framerate=(fraction)10/1 ! nvvidconv flip-method=0 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
+    cap = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)480, height=(int)320,format=(string)NV12, framerate=(fraction)5/1 ! nvvidconv flip-method=0 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
     print("camera captured")
 
     while True:
@@ -42,16 +42,20 @@ if __name__ == '__main__':
             res_img = cv2.addWeighted(img, 0.6, draw_person_pose(img, person_pose_array), 0.4, 0)
 
         discussant_pose_array = []
-        for speaker_id, person_pose in enumerate(person_pose_array):
+        discussant_status_dict = {}
+
+        for speaker_id,person_pose in enumerate(person_pose_array):
             unit_length = pose_detector.get_unit_length(person_pose)
+            print("speaker_id:", speaker_id, "は，unit_length:", unit_length)
             if 15 < unit_length < 1000:
                 discussant_pose_array.append(person_pose)
+
         discussant_pose_array = sorted(discussant_pose_array, key=lambda x: x[0][0])
 
-        for speaker_id, person_pose in enumerate(discussant_pose_array):
+        for speaker_id,person_pose in enumerate(discussant_pose_array):
             unit_length = pose_detector.get_unit_length(person_pose)
             if args.mode == "camera":
-                print(tuple(person_pose[0][:2]))
+                print("id書き込む座標:",tuple(person_pose[0][:2]))
                 cv2.putText(res_img, str(speaker_id), tuple(map(int,person_pose[0][:2])),cv2.FONT_HERSHEY_SIMPLEX,0.8, (0, 0, 0), lineType=cv2.LINE_AA)
 
             # hands estimation
@@ -75,19 +79,21 @@ if __name__ == '__main__':
                 hand_gesture_right = right_gesture_recognizer(hand_keypoints,unit_length)
                 if args.mode == "camera":
                     res_img = draw_gesture(res_img, hand_gesture_right, tuple(map(int,(person_pose[4][0], person_pose[4][1]))))
-            print("speaker_id:",speaker_id," は，","右手: ",hand_gesture_right," 左手: ",hand_gesture_left,"です")
-            student_status = get_student_status(hand_gesture_left,hand_gesture_right)
-            message = """
-            data: "{0},{1}" 
-            """.format(speaker_id+1,student_status)
-            query = {"message":message,'topic_name':'/printeps/std_msgs/update_student_status'}
-            try:
-                requests.post("http://localhost:8080/publish",data=query)
-            except Exception as e:
-                print(e)
+            print("speaker_id:", speaker_id, " は，", "右手: ", hand_gesture_right, " 左手: ", hand_gesture_left, "です")
+            student_status = get_student_status(hand_gesture_left, hand_gesture_right)
+            discussant_status_dict[str(speaker_id)] = "{0},{1}".format(speaker_id+1,student_status)
 
+        message = """
+        data: "{0}" 
+        """.format(json.dumps(discussant_status_dict))
+        query = {"message": message, 'topic_name': '/printeps/std_msgs/update_student_status'}
+        try:
+            requests.post("http://localhost:8080/publish", data=query)
+        except Exception as e:
+            print(e)
         if args.mode == "camera":
             cv2.imshow("result", res_img)
-        #time.sleep(0.1)
         cv2.waitKey(10)
+
+
 
